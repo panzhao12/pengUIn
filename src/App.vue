ac69deac947154189cd0b4f5d6df70c081f729f2
Pan <panzhao12@gmail.com>
2022-11-01 11:49:12 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import Mainstage from './components/MainStage.vue';
+</script>
+
+<template>
+  <header>
+    <div>pengUIn</div>
+  </header>
+
+  <main>
+    <Mainstage />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  display: flex;
+  place-items: flex-start;
+  flex-wrap: wrap;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+}
+</style>
