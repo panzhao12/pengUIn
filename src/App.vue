4e57b9858331b5472161a52b428dcbcf81e669db
Pan <panzhao12@gmail.com>
2022-11-07 23:49:51 +0100

    Create general layout and basic functionality for generating messages in random block.

--- src/App.vue
+++ src/App.vue
@@ -3,27 +3,7 @@ import MainStage from './components/MainStage.vue';
 </script>
 
 <template>
-  <header>
-    <div>pengUIn</div>
-  </header>
-  <main>
-    <MainStage />
-  </main>
+  <MainStage />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  display: flex;
-  place-items: flex-start;
-  flex-wrap: wrap;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-}
-</style>
+<style scoped></style>
--- src/components/MainStage.vue
+++ src/components/MainStage.vue
@@ -2,15 +2,95 @@
 import { ref } from 'vue';
 import TextSticker from './TextSticker.vue';
 
-const count = ref(6);
+const messagesB1 = ref<Array<string>>([]);
+const messagesB3 = ref<Array<string>>([]);
+const messagesB4 = ref<Array<string>>([]);
 
-function call() {
-  count.value++;
+function addMsg() {
+  const messageBlock = Math.floor(Math.random() * 3);
+  switch (messageBlock) {
+    case 0:
+      messagesB1.value.push('olga');
+      break;
+    case 1:
+      messagesB3.value.push('olga');
+      break;
+    case 2:
+      messagesB4.value.push('olga');
+      break;
+  }
 }
 </script>
 
 <template>
-  <div v-for="n in 6" :key="n">
-    <TextSticker :count="count" @add="call" />
+  <div id="b1">
+    <h2>Block1</h2>
+    <ul>
+      <li v-for="(msg, index) in messagesB1" :key="index">
+        {{ msg }}
+      </li>
+    </ul>
+  </div>
+
+  <div id="b2-5">
+    <div id="penguin">
+      <img src="p.png" alt="penguin" @click="addMsg" />
+    </div>
+    <div id="piano">
+      <img src="piano.png" alt="piano" />
+    </div>
+  </div>
+
+  <div id="b3">
+    <h2>Block3</h2>
+    <ul>
+      <li v-for="(msg, index) in messagesB3" :key="index">
+        {{ msg }}
+      </li>
+    </ul>
+  </div>
+
+  <div id="b4">
+    <h2>Block4</h2>
+    <ul>
+      <li v-for="(msg, index) in messagesB4" :key="index">
+        {{ msg }}
+      </li>
+    </ul>
   </div>
 </template>
+
+<style scoped>
+#b1 {
+  grid-row-end: span 2;
+}
+
+#b2-5 {
+  grid-row-end: span 2;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  align-self: center;
+}
+
+#b3 {
+  grid-row-end: span 2;
+}
+
+#b4 {
+  grid-column-end: span 3;
+}
+
+div {
+  display: inline-grid;
+}
+
+#penguin img {
+  width: 10rem;
+  align-self: center;
+}
+
+#piano img {
+  width: 30rem;
+}
+</style>
