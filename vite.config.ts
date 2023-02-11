4941392652a8c82aab58464f75cbb3c8b0403980
Pan <panzhao12@gmail.com>
2023-02-11 23:42:44 +0100

    Vite config update

--- vite.config.ts
+++ vite.config.ts
@@ -11,5 +11,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: '/penguin/' //TODO './' ??
+  base: './penguin/' //TODO './' ??
 });
