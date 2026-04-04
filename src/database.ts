3a60f1805c520f43149da80505f142b9dfdba7ab
Pan <panzhao12@gmail.com>
2026-04-04 10:39:04 +0200

    Database config updates

--- src/database.ts
+++ src/database.ts
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './dbTypes';
 
-const supabaseUrl = 'https://xwajagoyuqwubxjydant.supabase.co';
-const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh3YWphZ295dXF3dWJ4anlkYW50Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzYxNzQ1NDYsImV4cCI6MjA1MTc1MDU0Nn0.d7vcjZnoHqK-7m16QHUjSoruQv0TVqqJoLs6zcELfKU';
+const supabaseUrl = 'https://hxxvlibjifzjaxueocms.supabase.co';
+const supabaseKey = 'sb_publishable_3qkX4ySUDscqh_USY2zlPA_KhiMzpYE';
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseKey);
