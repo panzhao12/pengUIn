be5132cef421d57a8f2e3935644cac9fab9d119f
Pan <panzhao12@gmail.com>
2026-04-05 12:21:52 +0200

    Add production index.html and configure manual chunk splitting for vendor bundles in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,5 +11,18 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
+  build: {
+    chunkSizeWarningLimit: 800,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('tone')) return 'vendor-tone';
+            return 'vendor';
+          }
+        }
+      }
+    }
+  },
   base: '/pengUIn/' //TODO './' ??
 });
